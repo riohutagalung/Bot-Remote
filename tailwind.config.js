b361ccd6dd1f8ffbf9163db4d6dad0ed4b385bfb
Rio Hutagalung <rio.hutagalung2@gmail.com>
2026-05-04 11:52:30 +0000

    Fix Vercel config for Vite deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
-    './App.jsx',
   ],
   theme: {
     container: {
